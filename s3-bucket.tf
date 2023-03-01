793875e5745126efac61fb994beb598c8a738d37
root <testpinaki@gmail.com>
2023-03-01 20:40:46 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,4 +1,11 @@
-module "s3-bucket" {
-  source  = "app.terraform.io/pinaki-hcp/s3-bucket/aws"
-  version = "2.8.0"
+module "s3_bucket" {
+  source = "terraform-aws-modules/s3-bucket/aws"
+
+  bucket = "my-s3-bucket"
+  acl    = "private"
+
+  versioning = {
+    enabled = true
+  }
+
 }
